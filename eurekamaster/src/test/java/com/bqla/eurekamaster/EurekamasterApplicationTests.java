700a6c7900e7458b0eb929530e8bd5c9c0e938b9
vincent <592731889@qq.com>
2019-11-17 00:17:39 +0800

    eureka

---
+++ eurekamaster/src/test/java/com/bqla/eurekamaster/EurekamasterApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bqla.eurekamaster;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekamasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
